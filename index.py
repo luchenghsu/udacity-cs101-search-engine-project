7b34d6ce556fbc66cf354b6827c030625b3b1623
luchenghsu <luchenghsu@gmail.com>
2015-01-29 10:10:05 -0800

    Implementation of Index in Python

---
+++ index.py
@@ -0,0 +1,46 @@
+def add_page_to_index(index, url, content): 
+    words = content.split() 
+    for word in words: 
+        add_to_index(index, word, url) 
+
+def add_to_index(index, keyword, url): 
+    if keyword in index:
+        index[keyword].append(url)
+    else:
+        index[keyword] = [url]     
+
+def lookup(index, keyword): 
+    if keyword in index:
+        return index[keyword]
+    return None
+   
+def compute_ranks(graph):
+    d = 0.8 # damping factor
+    numloops = 10
+    
+    ranks = {}
+    npages = len(graph)
+    for page in graph:
+        ranks[page] = 1.0 / npages
+    
+    for i in range(0, numloops):
+        newranks = {}
+        for page in graph:
+            newrank = (1 - d) / npages
+           
+            for node in graph:
+                if page in graph[node]:
+                    newrank = newrank + d * (ranks[node] / len (graph[node]))
+                    
+            newranks[page] = newrank
+        ranks = newranks
+    return ranks
+    
+"""
+rank(page, 0) = 1/npages
+
+   rank(page, t) = (1-d)/npages 
+
+                   + sum (d * rank(p, t - 1) / number of outlinks from p) 
+              over all pages p that link to this page 
+"""   
